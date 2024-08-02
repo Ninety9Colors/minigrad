d48db0a99c25130e42de78ffb3daf4fc4953d2a6
Ninety9Colors <darekzhang0629@gmail.com>
2024-08-02 17:49:35 -0500

    Implemented backpropagation through a Value and its children
    Calculates derivative values for all children with respect to a single parent
    
    Does not support Values raised to Values, however. Only raised to int/float

--- value.py
+++ value.py
@@ -6,10 +6,46 @@ class Value:
     def __init__(self, value, children=(), op=''):
         self.data = value
         self.grad = 0
-        self.respect = None
+        self.respect_to = None
         
         self.children = children
         self.op = op
+    
+    def calc_grad(self, respect_to=None):
+        if self.children:
+            a,b = self.children
+            if self.op == '+':
+                a.grad += self.grad
+                b.grad += self.grad
+
+                a.respect_to = respect_to
+                b.respect_to = respect_to
+            elif self.op == '*':
+                a.grad += b.data*self.grad
+                b.grad += a.data*self.grad
+
+                a.respect_to = respect_to
+                b.respect_to = respect_to
+            elif self.op == '**':
+                a.grad += (b*a.data**(b-1))*self.grad
+
+                a.respect_to = respect_to
+    def backward(self):
+        topo = []
+        visited = set()
+        def topo_sort(nodes):
+            for n in nodes:
+                if isinstance(n, Value):
+                    if n not in visited:
+                        visited.add(n)
+                        topo_sort(n.children)
+                        topo.append(n)
+        topo_sort(self.children)
+        topo.append(self)
+        
+        self.grad = 1
+        for n in reversed(topo):
+            n.calc_grad(self)
 
     def __repr__(self):
         return f'Value(data={self.data}, grad={self.grad})'
@@ -23,8 +59,8 @@ class Value:
         return Value(self.data * other.data, (self, other), '*')
 
     def __pow__(self, o):
-        other = o if isinstance(o, Value) else Value(o)
-        return Value(self.data ** other.data, (self, other), '**')
+        assert isinstance(o, (int, float))
+        return Value(self.data ** o, (self, o), '**')
 
     def __neg__(self):
         return self * -1
