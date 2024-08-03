233ca8cafe1d2e1fac5a85095b1e7f5c2cf1f2e5
Ninety9Colors <darekzhang0629@gmail.com>
2024-08-03 17:23:42 -0500

    Implemented exponential function support for derivatives
    Uses natural log function estimation

--- value.py
+++ value.py
@@ -27,9 +27,11 @@ class Value:
                 a.respect_to = respect_to
                 b.respect_to = respect_to
             elif self.op == '**':
-                a.grad += (b*a.data**(b-1))*self.grad
+                a.grad += (b.data*a.data**(b.data-1))*self.grad
+                b.grad += (self.ln(a.data)*a.data**b.data)*self.grad
 
                 a.respect_to = respect_to
+                b.respect_to = respect_to
     def backward(self):
         topo = []
         visited = set()
@@ -46,6 +48,12 @@ class Value:
         self.grad = 1
         for n in reversed(topo):
             n.calc_grad(self)
+    
+    # Natural log approximation
+    def ln(self, o):
+        o = self.data if o == None else o
+        assert(o>0)
+        return 99999999*(o**(1/99999999)-1)
 
     def __repr__(self):
         return f'Value(data={self.data}, grad={self.grad})'
@@ -59,8 +67,12 @@ class Value:
         return Value(self.data * other.data, (self, other), '*')
 
     def __pow__(self, o):
-        assert isinstance(o, (int, float))
-        return Value(self.data ** o, (self, o), '**')
+        other = o if isinstance(o, Value) else Value(o)
+        return Value(self.data ** other.data, (self, other), '**')
+
+    def __rpow__(self, o):
+        other = o if isinstance(o, Value) else Value(o)
+        return Value(other.data ** self.data, (other, self), '**')
 
     def __neg__(self):
         return self * -1
