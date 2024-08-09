3c16c77cf7afbd6be398cd97358efdc6fcd99ee5
Ninety9Colors <darekzhang0629@gmail.com>
2024-08-09 11:35:03 -0500

    Refactoring and commenting of neural.py
    Changed predict functions to python's __call__ function for easier use
    
    Merged single_predict function into the base calculation for MLP

--- neural.py
+++ neural.py
@@ -1,6 +1,7 @@
 from value import Value
 import random
 
+# Utility function, splits data into two lists
 def train_test_split(data, percent_train, shuffle=True):
     if shuffle:
         random.seed()
@@ -9,6 +10,7 @@ def train_test_split(data, percent_train, shuffle=True):
 
     return data[:int(n*percent_train)], data[int(n*percent_train):]
 
+# Holds weight vector and single bias, with activation function and randomized initial weights
 class Neuron:
     def __init__(self, input_size, seed=None, activation=None):
         if seed:
@@ -16,13 +18,9 @@ class Neuron:
         self.w = [Value(random.uniform(-1 ,1)) for _ in range(input_size)]
         self.b = Value(random.uniform(-1, 1))
         self.activation = activation
-    
-    def __repr__(self, verbose=False):
-        if verbose:
-            return f"Neuron(input_size={len(self.w)}, activation={self.activation}, w={[v.data for v in self.w]}, b={self.b.data})"
-        return f"Neuron(input_size={len(self.w)}, activation={self.activation})"
-    
-    def predict(self, x_input):
+
+    # Feeds forward input vector and returns scalar Value result
+    def __call__(self, x_input: list[Value]) -> Value:
         assert len(x_input) == len(self.w)
 
         raw = sum((x_input[i]*self.w[i] for i in range(len(self.w))), self.b)
@@ -36,16 +34,34 @@ class Neuron:
         elif self.activation == 'sigmoid':
             return raw.sigmoid()
 
-    def parameters(self):
+    # Returns concatenated list of all weights (w+b)
+    def parameters(self) -> list[Value]:
         return self.w + [self.b]
     
-
+    def __repr__(self, verbose=False):
+        if verbose:
+            return f"Neuron(input_size={len(self.w)}, activation={self.activation}, w={[v.data for v in self.w]}, b={self.b.data})"
+        return f"Neuron(input_size={len(self.w)}, activation={self.activation})"
+    
+# List of {output_size} neurons of size {input_size}
 class Layer:
     def __init__(self, input_size, output_size, seed=None, activation=None):
         if seed:
             random.seed(seed)
         self.neurons = [Neuron(input_size, activation=activation) for _ in range(output_size)]
-    
+
+    # Returns activation vector for a vector input x_input
+    def __call__(self, x_input: list[Value]) -> list[Value]:
+        assert len(x_input) == len(self.neurons[0].w)
+
+        return [n(x_input) for n in self.neurons]
+
+    # Returns concatenated list of weights of each neuron
+    def parameters(self) -> list[Value]:
+        result = []
+        for n in self.neurons: result.extend(n.parameters())
+        return result
+
     def __repr__(self, verbose=False):
         if verbose:
             result = f"Layer(input_size={len(self.neurons[0].w)}, output_size={len(self.neurons)},\n"
@@ -54,29 +70,59 @@ class Layer:
             return result + ')'
         return f"Layer(input_size={len(self.neurons[0].w)}, output_size={len(self.neurons)}, activation={self.activation})"
 
-    def predict(self, x_input):
-        assert len(x_input) == len(self.neurons[0].w)
-
-        return [n.predict(x_input) for n in self.neurons]
-
-    def parameters(self):
-        result = []
-        
-        for n in self.neurons:
-            result.extend(n.parameters())
-
-        return result
-
+# List of layers of multiple sizes and activations, uses MSE cost by default, but supports user defined cost functions
 class MLP:
     def __init__(self, input_size, layer_sizes, cost = None, seed=None, inner_activation=None, final_activation=None):
         random.seed(seed)
-        sizes = [input_size]+layer_sizes
-        if cost:
-            self.cost = cost
-        else:
-            self.cost = self.mse
-        self.layers = [Layer(sizes[i-1], sizes[i], activation=inner_activation) for i in range(1, len(sizes)-1)]
-        self.layers.append(Layer(sizes[-2], sizes[-1], activation=final_activation))
+        layer_sizes = [input_size]+layer_sizes
+
+        # Appends each internal and input layer
+        self.layers = [Layer(layer_sizes[i-1], layer_sizes[i], activation=inner_activation) for i in range(1, len(layer_sizes)-1)]
+
+        # Appends output layer with final activation
+        self.layers.append(Layer(layer_sizes[-2], layer_sizes[-1], activation=final_activation))
+
+        if cost: self.cost = cost
+        else: self.cost = self.mse
+    
+    # Returns matrix result of forward propagation for multiple input vectors in x_input
+    def __call__(self, x_input: list[list[Value]]) -> list[list[Value]]:
+        assert len(x_input[0]) == len(self.layers[0].neurons[0].w)
+
+        # Returns vector result of forward propagation for a single input vector x_input
+        def single_predict(x_input: list[Value]) -> list[Value]:
+            assert len(x_input) == len(self.layers[0].neurons[0].w)
+            # Start by calculating input layer activation
+            activation = self.layers[0](x_input)
+
+            # Feed forward activation through layers
+            for i in range(1, len(self.layers)):
+                activation = self.layers[i](activation)
+            return activation
+
+        return [single_predict(example) for example in x_input]
+    
+    # Default cost function for MLP, returns squared difference between each predicted and target output value
+    # By default uses L2 regularization, can be turned off by setting lambda_ = 0
+    def mse(self, x_input: list[list[Value]], y_target: list[list[Value]], lambda_=1e-4):
+        assert len(x_input[0]) == len(self.layers[0].neurons[0].w)
+        assert len(y_target[0]) == len(self.layers[len(self.layers)-1].neurons)
+
+        # MSE Loss
+        y_predict = self(x_input)
+        loss = sum((y_predict[i][j]-y_target[i][j])**2 for j in range(len(y_target[0])) for i in range(len(y_target)))
+
+        # L2 Loss
+        reg_loss = lambda_ * sum((p*p for p in self.parameters()))
+
+        # Return total loss
+        return loss + reg_loss
+    
+    # Returns concatenated list of weights of each layer and its neurons
+    def parameters(self) -> list[Value]:
+        result = []
+        for l in self.layers:result.extend(l.parameters())
+        return result
     
     def __repr__(self, verbose=False):
         if verbose:
@@ -85,37 +131,3 @@ class MLP:
                 result += l.__repr__(True) + '\n'
             return result + ')'
         return f"MLP(input_size={len(self.layers[0].neurons[0].w)}, layer_sizes={[len(self.layers[i].neurons) for i in range(0, len(self.layers))]})"
-    
-    def predict(self, x_input):
-        assert len(x_input[0]) == len(self.layers[0].neurons[0].w)
-
-        return [self.single_predict(example) for example in x_input]
-    
-    def single_predict(self, x_input):
-        assert len(x_input) == len(self.layers[0].neurons[0].w)
-
-        activation = self.layers[0].predict(x_input)
-
-        for i in range(1, len(self.layers)):
-            activation = self.layers[i].predict(activation)
-        
-        return activation
-    
-    def mse(self, x_input, y_target, alpha=1e-4):
-        assert len(x_input[0]) == len(self.layers[0].neurons[0].w)
-        assert len(y_target[0]) == len(self.layers[len(self.layers)-1].neurons)
-
-        y_predict = [self.single_predict(i) for i in x_input]
-        loss = sum((y_predict[i][j]-y_target[i][j])**2 for j in range(len(y_target[0])) for i in range(len(y_target)))
-
-        reg_loss = alpha * sum((p*p for p in self.parameters()))
-        
-        return loss + reg_loss
-    
-    def parameters(self):
-        result = []
-        
-        for l in self.layers:
-            result.extend(l.parameters())
-
-        return result
